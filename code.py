4b765ed4679a4aeb955dcd1f615e61ce2a42e352
Bakhriddinov Jamoliddin <91607120+anqoVoube@users.noreply.github.com>
2022-04-20 02:04:51 +0500

    First commit

---
+++ code.py
@@ -0,0 +1,28 @@
+class ReadableBalance:
+    def __init__(self, balance: float) -> str:
+        self.balance = balance
+        self.return_balance = str(balance)
+
+    def decimal_digits(self, length: int = 2):
+        format_length = f".{length}f"
+        self.return_balance = format(self.balance, format_length)
+        return self
+
+    def dots(self):
+        if self.return_balance.find('.') != -1:
+            int_decimal_sep =  self.return_balance.split('.')
+            value_wout_decimals = int(int_decimal_sep[0])
+            self.return_balance = f'{value_wout_decimals:,}' + '.' + str(int_decimal_sep[1])
+        else:
+            self.return_balance = f'{self.balance:,}'
+        
+        return self.return_balance
+
+    @property
+    def display(self):
+        return self.return_balance
+
+
+#x = ReadableBalance(5000)
+#print(x.decimal_digits().display)
+
